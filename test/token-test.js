95f3cbdc14266398565f6d217f342df8f6b4bff6
topbot <Topbot1995@gmail.com>
2022-04-05 14:06:29 +0300

    update the contract

--- test/token-test.js
+++ test/token-test.js
@@ -4,7 +4,7 @@ const { ethers } = require("hardhat");
 describe("CroToken ", function () {
   it("Private Sale test", async function () {
     const [owner, addr1, addr2] = await ethers.getSigners();
-    const CroToken = await ethers.getContractFactory("CroToken");
+    const CroToken = await ethers.getContractFactory("CroToken2");
     const croToken = await CroToken.deploy();
     await croToken.deployed();
     const listingPrice = ethers.utils.parseUnits('750000', 'ether');
@@ -30,18 +30,5 @@ describe("CroToken ", function () {
     // expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 
-  it("Private Sale test", async function () {
-    const [owner, addr1, addr2] = await ethers.getSigners();
-    const CroToken = await ethers.getContractFactory("CroToken1");
-    const croToken = await CroToken.deploy();
-    await croToken.deployed();
-    const listingPrice = ethers.utils.parseUnits('75', 'ether');
-    await croToken.setWhiteList([owner.address, addr1.address], [listingPrice, listingPrice]);
-    await croToken.setPrivateTgeTime('121234');
-    console.log(await croToken.private_locks(owner.address));    
-    console.log(await croToken.private_released(owner.address));    
-    await croToken.connect(owner).privateClaim();
-    console.log(await croToken.balanceOf(owner.address));  
-    console.log(croToken.address);
-  });
+
 });
